97b709bb86c817a7cbb8ed0a3f9bfd71f0e64f6f
SaraSorlin <sorlin82@gmail.com>
2023-11-19 15:58:28 +0100

    hantering registrera användare

--- script.js
+++ script.js
@@ -11,6 +11,58 @@ let selectedMarkerIndex = -1;
 let gameOver = false;
 
 
+function registerUser() {
+  const newPlayer = document.getElementById('newPlayer').value;
+  if (!newPlayer) {
+    alert('Ange ett användarnamn för att registrera.');
+    return;
+  }
+
+  fetchUsers().then(users => {
+    if (users.some(user => user.username === newPlayer)) {
+      alert('Användarnamnet är redan registrerat.');
+      return;
+    }
+
+    const newUser = {
+      username: newPlayer,
+      matchHistory: [],
+    };
+
+    saveUser(newUser).then(() => {
+      alert(`Användaren "${newPlayer}" har registrerats.`);
+      updatePlayerSelections();
+    });
+  });
+}
+
+function saveUser(user) {
+  return fetch('http://localhost:3000/users/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  }).then(response => response.json());
+}
+
+function fetchUsers() {
+  return fetch('http://localhost:3000/users/')
+    .then(response => response.json())
+    .catch(err => console.error('Error fetching users:', err));
+}
+
+function updateUser(user) {
+  return fetch(`http://localhost:3000/users/${user.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  }).then(response => response.json());
+}
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('startGameBtn').addEventListener('click', startGame);
